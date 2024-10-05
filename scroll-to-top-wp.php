35c3a54706064f676550c856bec935a0437feb72
nasim114 <nasimuddin1140@gmail.com>
2024-10-06 00:14:32 +0600

    plugin description

--- scroll-to-top-wp.php
+++ scroll-to-top-wp.php
@@ -1,5 +1,16 @@
 <?php
-
-
+/**
+*Plugin Name:       Scroll To Top WP
+*Plugin URI:        https://wordpress.org/plugins/scroll-to-top-wp/
+*Description:       Scroll to top plugin will help you to enable Back to Top button to your WordPress website.
+*Version:           1.0.0
+*Requires at least: 5.8
+*Requires PHP:      7.4
+*Author:            Luminous Labs BD
+*Author URI:        https://luminouslabsbd.com
+*Update URI:        https://github.com/Nasim25/scroll-to-top-wp-plugin
+*License:           GPLv2 or later
+*Text Domain:       sttwp
+*/
 
 ?>
