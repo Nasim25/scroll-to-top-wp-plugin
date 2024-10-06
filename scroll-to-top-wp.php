0305ef1532d4a4d319abce50b22d0a296ad164c3
nasim114 <nasimuddin1140@gmail.com>
2024-10-06 12:48:15 +0600

    enabed plugin script

--- scroll-to-top-wp.php
+++ scroll-to-top-wp.php
@@ -10,7 +10,29 @@
 *Author URI:        https://luminouslabsbd.com
 *Update URI:        https://github.com/Nasim25/scroll-to-top-wp-plugin
 *License:           GPLv2 or later
-*Text Domain:       sttwp
+*Text Domain:       llsttwp
 */
 
+// include css
+function llsttwp_enqueue_style(){
+    wp_enqueue_style('llsttwp-style', plugins_url('css/llsttwp-style.css', __FILE__));
+}
+add_action( 'wp_enqueue_scripts', 'llsttwp_enqueue_style');
+
+// include javaScript
+function llsttwp_enqueue_script(){
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('llsttwp-script', plugins_url('js/llsttwp-plugin.js', __FILE__),array(),'1.0.0',true);
+}
+add_action( 'wp_enqueue_scripts', 'llsttwp_enqueue_script');
+
+function llsttwp_scroll_script(){?>
+    <script>
+        jQuery(document).ready(function(){
+            jQuery.scrollUp();
+        });
+    </script>
+    <?php
+}
+add_action("wp_footer","llsttwp_scroll_script")
 ?>
